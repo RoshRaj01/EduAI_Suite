73c169dd9f0b2089ef123160e45671343bd48681
OCTehckie18 <omkar.chakraborty2001@gmail.com>
2026-04-13 16:33:58 +0530

    feature: full built of frontend is initialized

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
+import { router } from './router'
+
+import { ThemeProvider } from './shared/hooks/useTheme'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>,
+)
